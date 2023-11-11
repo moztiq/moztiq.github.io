22823b411032187b54804d52477ca2c2319813f4
Seongki Kim <moztiq@gmail.com>
2023-11-11 15:23:50 +0900

    gatsby init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,28 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
+require('dotenv').config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `moztiq.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://moztiq.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "6tD7q9MD3Ifw56GrvDYZGSb6kqAVbSy2AGK6USSUjlM",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'vpyfljks33op',
+        accessToken: process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+  ],
 };
 
 export default config;
